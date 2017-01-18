86c4558ca1b74d49072752cb328b638c7b9d2b7f
Kengo Nakajima <kengo.nakajima@gmail.com>
2017-01-18 12:36:41 +0900

    worked

--- test.js
+++ test.js
@@ -1,87 +1,90 @@
-
-
-/*
-typedef enum {
-  SNAPPY_OK = 0,
-  SNAPPY_INVALID_INPUT = 1,
-  SNAPPY_BUFFER_TOO_SMALL = 2
-} snappy_status;
-
-
-snappy_status snappy_compress(const char* input,
-                              size_t input_length,
-                              char* compressed,
-                              size_t* compressed_length);
-
-snappy_status snappy_uncompress(const char* compressed,
-                                size_t compressed_length,
-                                char* uncompressed,
-                                size_t* uncompressed_length);
-
-size_t snappy_max_compressed_length(size_t source_length);
-
-snappy_status snappy_uncompressed_length(const char* compressed,
-                                         size_t compressed_length,
-                                         size_t* result);
-
-snappy_status snappy_validate_compressed_buffer(const char* compressed, size_t compressed_length);
-*/
-
-
 var Module = require("./snappy.js")
 Module.snappy_compress = Module.cwrap("snappy_compress","number",["number","number","number","number"])
 Module.snappy_uncompress = Module.cwrap("snappy_uncompress","number",["number","number","number","number"])
 Module.snappy_max_compressed_length = Module.cwrap( "snappy_max_compressed_length","number",["number"] )
+Module.snappy_uncompressed_length = Module.cwrap( "snappy_uncompressed_length", "number", ["number","number","number"] );
 
 
-Module.max_compressed_length = function(inputsz) {
-    var retval = this.snappy_max_compressed_length(inputsz);
-    return retval;
-}
-
-// i8adata: Int8Array input
-function makeI8Heap(mod, i8adata) {
-    var nDataBytes = i8adata.length;
+function makeI8Heap(mod, ui8a) {
+    var nDataBytes = ui8a.length;
     var dataPtr = mod._malloc(nDataBytes);
     var dataHeap = new Uint8Array(mod.HEAPU8.buffer,dataPtr,nDataBytes);
-    dataHeap.set(new Uint8Array(i8adata.buffer));    
+    dataHeap.set(new Uint8Array(ui8a.buffer));    
     return dataHeap;
 }
-
+Module.getInt32FromHeap = function(bo) {
+    var b0 = this.getValue(bo+0,"i8") & 0xff;
+    var b1 = this.getValue(bo+1,"i8") & 0xff;
+    var b2 = this.getValue(bo+2,"i8") & 0xff;
+    var b3 = this.getValue(bo+3,"i8") & 0xff;
+    return b0+b1*256+b2*65536+b3*65536*256;
+}
+Module.setInt32ToHeap = function(bo,val) {
+    var b0 = val&0xff;
+    var b1 = (val/256)&0xff;
+    var b2 = (val/65536)&0xff;
+    var b3 = (val/65536/256)&0xff;
+    this.setValue(bo+0,b0,"i8");
+    this.setValue(bo+1,b1,"i8");
+    this.setValue(bo+2,b2,"i8");
+    this.setValue(bo+3,b3,"i8");    
+}
 //
-Module.compress = function(i8adata) {
-//    console.log("i8adata:",i8adata);
-    var input_heap = makeI8Heap(this,i8adata)
-    var outlen_max = this.max_compressed_length(i8adata.length);
-    console.log("outlen_max", outlen_max);
-    var outputbuf = new Int8Array(outlen_max);
-    var output_heap = makeI8Heap(this,outputbuf);
-    var outsizebuf = new Int8Array(4); // sizeof(size_t) is 4 in emscripten
-    var outsize_heap = makeI8Heap(this,outsizebuf);
-    this.setValue( outsize_heap.byteOffset,outlen_max, "i8")
-//    for(var i=0;i<i8adata.length;i++) console.log("input_heap:",i, this.getValue(input_heap.byteOffset+i,"i8"));
-//    for(var i=0;i<outlen_max;i++) console.log("output_heap:",i, this.getValue(output_heap.byteOffset+i,"i8"));
-    var retval = this.snappy_compress( input_heap.byteOffset, i8adata.length, output_heap.byteOffset, outsize_heap.byteOffset );
-    console.log("snappy_compress call retval:",retval);
-    for(var i=0;i<retval;i++) console.log( "output_heap:",i, this.getValue(output_heap.byteOffset+i,"i8"));
-    var i8aout = null
+Module.compress = function(ui8a) {
+    var input_heap = makeI8Heap(this,ui8a)
+    var outlen_max = this.snappy_max_compressed_length(ui8a.length);
+    var output_heap = makeI8Heap(this,new Uint8Array(outlen_max));
+    var outsize_heap = makeI8Heap(this,new Uint8Array(4)); // sizeof(size_t) is 4 in emscripten
+    this.setInt32ToHeap(outsize_heap.byteOffset,outlen_max);
+    var retval = this.snappy_compress( input_heap.byteOffset, ui8a.length, output_heap.byteOffset, outsize_heap.byteOffset );
+//    console.log("snappy_compress call retval:",retval, "output_heap.buffer:", output_heap.buffer );
+    var ui8aout = null
     if(retval==0) {
-        var b0 = this.getValue(outsize_heap.byteOffset+0,"i8") & 0xff;
-        var b1 = this.getValue(outsize_heap.byteOffset+1,"i8") & 0xff;
-        var b2 = this.getValue(outsize_heap.byteOffset+2,"i8") & 0xff;
-        var b3 = this.getValue(outsize_heap.byteOffset+3,"i8") & 0xff;
-        var outlen = b0+b1*256+b2*65536+b3*65536*256;
-        console.log("compressed outlen:",outlen);
-        for(var i=0;i<outlen;i++) console.log("output_heap:",i, this.getValue(output_heap.byteOffset+i,"i8"));        
-        i8aout = new Int8Array(output_heap.buffer, output_heap.byteOffset, outlen);
+        var outlen = this.getInt32FromHeap(outsize_heap.byteOffset);
+        ui8aout = new Uint8Array(outlen); // dont use new Uint8Array(buf,ofs,outlen); buffer.byteLength gets 16M dont know why
+        for(var i=0;i<outlen;i++) ui8aout[i] = this.getValue(output_heap.byteOffset+i,"i8");
+    } else {
+        console.log("snappy_compress failed, retval:",retval);
     }
     
     this._free(outsize_heap.byteOffset);
     this._free(output_heap.byteOffset);
     this._free(input_heap.byteOffset);
-    return i8aout;
+    return ui8aout;
 }
-Module.uncompress = 
+Module.uncompressed_length = function(ui8a) {
+    var input_heap = makeI8Heap(this,ui8a);
+    var result_heap = makeI8Heap(this,new Uint8Array(4));
+    var retval = this.snappy_uncompressed_length( input_heap.byteOffset, ui8a.length, result_heap.byteOffset );
+    var uncomp_size = this.getInt32FromHeap(result_heap.byteOffset);
+    this._free(input_heap.byteOffset);
+    this._free(result_heap.byteOffset);    
+    if(retval!=0) {
+        return -1;
+    } else {
+        return uncomp_size;
+    }        
+}
+Module.uncompress = function(ui8a) {
+    var uncomp_len = this.uncompressed_length(ui8a);
+    var input_heap = makeI8Heap(this,ui8a);    
+    var output_heap = makeI8Heap(this,new Uint8Array(uncomp_len));
+    var outsize_heap = makeI8Heap(this,new Uint8Array(4));
+    this.setInt32ToHeap(outsize_heap.byteOffset,uncomp_len);
+    var retval = this.snappy_uncompress( input_heap.byteOffset, ui8a.length, output_heap.byteOffset, outsize_heap.byteOffset );
+    var ui8aout = new Uint8Array(uncomp_len);
+    for(var i=0;i<uncomp_len;i++) ui8aout[i] = this.getValue( output_heap.byteOffset+i,"i8");
+
+    this._free(input_heap.byteOffset);
+    this._free(output_heap.byteOffset);
+    this._free(outsize_heap.byteOffset);
+    if(retval!=0) {
+        console.log("snappy_uncompress failed:",retval);
+        return null;
+    } else {
+        return ui8aout;        
+    }
+}
 
 function compareArray(a,b) {
     if(a.length!=b.length) return false;
@@ -96,7 +99,7 @@ function check(a) {
     if( compareArray(uncompressed,a) == false ) {
         console.log("no match!",uncompressed,a);
     } else {
-        console.log("OK for ",a);
+        console.log("OK for data length:",a.length);
     }
 }
         
@@ -106,6 +109,23 @@ function check(a) {
 ///////////////////
 
 
-var a0 = new Int8Array([11,22,33,44,55,4,44,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,4,4,4,44,4,4,4,4,4,4,4,44,4,4,4,4,4,4,4,4,4,4,4,44,4,44,44,44,44,44,44,44,44,4,4,4,4]); 
+var a0 = new Uint8Array([11,22,33,44,55]);
 check(a0);
 
+var a1 = new Uint8Array([11,22,33,44,55,4,44,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,44,4,4,4,4,44,4,4,4,4,4,4,4,4,4,44,4,4,4,4,4,4,4,44,4,4,4,4,4,4,4,4,4,4,4,44,4,44,44,44,44,44,44,44,44,4,4,4,4]); 
+check(a1);
+
+var a2 = new Uint8Array(1000);
+for(var i=0;i<1000;i++) a2[i] = Math.random() * 255;
+check(a2);
+
+var a3 = new Uint8Array(10000);
+for(var i=0;i<10000;i++) a3[i] = Math.random() * 255;
+check(a3);
+
+var a4 = new Uint8Array(100000);
+for(var i=0;i<100000;i++) a4[i] = Math.random() * 255;
+var st = new Date().getTime();
+check(a4);
+var et = new Date().getTime();
+console.log("time for", a4.length, ":", (et-st), "ms");
